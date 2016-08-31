e97c7a6c38d5728e0c70c0f1064947735a348e20
Jyunri <nekojimy@gmail.com>
2016-08-31 03:19:22 -0300

    bottom bar

--- app/src/main/java/br/com/cdf/pizzariadocheff/MainActivity.java
+++ app/src/main/java/br/com/cdf/pizzariadocheff/MainActivity.java
@@ -2,44 +2,72 @@ package br.com.cdf.pizzariadocheff;
 
 import android.content.Intent;
 import android.net.Uri;
+
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabReselectListener;
+import com.roughike.bottombar.OnTabSelectListener;
 
-    Button btCardapio, btContato;
-    Button btLigar;
+
+public class MainActivity extends AppCompatActivity{
+
+    boolean firstAccess;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btCardapio = (Button)findViewById(R.id.btCardapio);
-        btContato = (Button)findViewById(R.id.btContato);
-        btLigar = (Button)findViewById(R.id.btLigar);
+        firstAccess = true;
+        BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
+        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelected(@IdRes int tabId) {
+                switch (tabId) {
+                    //// TODO: 8/31/16 melhorar para o primeiro acesso
+                    case (R.id.tab_ligar):
+                        if(firstAccess) firstAccess = false;
+                        else {
+                            Intent callIntent = new Intent(Intent.ACTION_DIAL);
+                            callIntent.setData(Uri.parse("tel:123456789"));
+                            startActivity(callIntent);
+                        }
+                        break;
+                    case (R.id.tab_cardapio):
+                        startActivity(new Intent(getApplicationContext(), Cardapio.class));
+                        break;
+                    case (R.id.tab_localizacao):
+                        break;
+                    case (R.id.tab_facebook):
+                        break;
+                }
+            }
+        });
 
-        btCardapio.setOnClickListener(this);
-        btContato.setOnClickListener(this);
-        btLigar.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId())
-        {
-            case R.id.btCardapio:
-                startActivity(new Intent(getApplicationContext(),Cardapio.class));
-                break;
-            case R.id.btContato:
-                break;
-            case R.id.btLigar:
-                Intent callIntent = new Intent(Intent.ACTION_DIAL);
-                callIntent.setData(Uri.parse("tel:123456789"));
-                startActivity(callIntent);
-                break;
-        }
+        bottomBar.setOnTabReselectListener(new OnTabReselectListener() {
+            @Override
+            public void onTabReSelected(@IdRes int tabId) {
+                switch (tabId) {
+                    //// TODO: 8/31/16 melhorar para o primeiro acesso
+                    case (R.id.tab_ligar):
+                        Intent callIntent = new Intent(Intent.ACTION_DIAL);
+                        callIntent.setData(Uri.parse("tel:123456789"));
+                        startActivity(callIntent);
+                        break;
+                    case (R.id.tab_cardapio):
+                        startActivity(new Intent(getApplicationContext(), Cardapio.class));
+                        break;
+                    case (R.id.tab_localizacao):
+                        break;
+                    case (R.id.tab_facebook):
+                        break;
+                }
+            }
+        });
     }
 }
